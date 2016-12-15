b5c93179b5723547bd0443d2330d461a0cdaaac4
Unknown <kiradust@gmail.com>
2016-12-15 19:53:55 +0200

    fig 5d in progress

--- fig5.py
+++ fig5.py
@@ -2,9 +2,11 @@ from plm_singlecomp_withkcc2 import plm, zp
 from plotting import minifig, minithreefig, minifigtwoaxes,twoaxes,xcolor,clcolor,wcolor,kcolor
 import matplotlib.pyplot as plt
 from pylab import rcParams
-import numpy as np
 from fig4 import f4e
+import numpy as np
 rcParams['figure.figsize'] = 8,8
+
+sym=['-b',':r','--g','-.m']
 
 def f5a():
     dez=plm(dz=1e-5,two=1,xt=10)
@@ -12,15 +14,24 @@ def f5a():
     return
 
 def f5b(moldelt=0):
-    XF=[-1.2,-1.1,-1.0,-0.9,-0.85,-0.8,-0.7,-0.6,-0.55]
+    #XF=[-1.2,-1.1,-1.0,-0.9,-0.85,-0.8,-0.7,-0.6,-0.55]
+    DZ=[0e-6,1e-6,2e-6,3e-6,4e-6]
     cl=[]
     vm=[]
     k=[]
     w=[]
     z=[]
     x=[]
-    for a in XF:
-        dez=plm(tt=200,z=a)
+    for a in DZ:
+        dez=plm(two=1,xt=10,dz=a,Zx=-1.19)
+        #minithreefig([dez[11][1:-1],dez[14][1:-1],dez[13][1:-1],dez[16][1:-1],dez[10][1:-1],dez[22][1:-1]],'k')
+        cl.append(dez[7])
+        k.append(dez[6])
+        vm.append(dez[9])
+        w.append(dez[10][-1])
+        z.append(dez[22][-1]*100)
+        x.append(dez[3])
+        dez=plm(two=1,xt=10,dz=a,Zx=-0.51)
         #minithreefig([dez[11][1:-1],dez[14][1:-1],dez[13][1:-1],dez[16][1:-1],dez[10][1:-1],dez[22][1:-1]],'k')
         cl.append(dez[7])
         k.append(dez[6])
@@ -38,9 +49,23 @@ def f5b(moldelt=0):
     return
 
 def f5c(ratio=0.8,md=1e-13):
-    dez=plm(gx=1e-8,xt=25,tt=200,two=1,xend=0,moldelt=md,ratio=ratio)
+    #dez=plm(gx=1e-8,xt=25,tt=200,two=1,xend=0,moldelt=md,ratio=ratio)
+    dez=plm(xend=0,two=1,xt=10,xflux=5e-6,moldelt=9e-12)
     minithreefig([dez[11][1:-1],dez[14][1:-1],dez[13][1:-1],dez[16][1:-1],dez[10][1:-1],dez[22][1:-1]],'k')
     return
 
 def f5d():
-    return
+    w=[[],[],[],[]]
+    z=[[],[],[],[]]
+    ZX=[-0.5,-1,-2,-3]
+    ZT=[-0.6,-0.85,-1,-1.5,-2.0,-2.5]
+    plt.figure()
+    for a in ZX:
+        for b in ZT:
+            dez=plm(xend=0,two=1,xt=10,xflux=5e-6,ztarget=b,Zx=a,moldelt=1)
+            #minithreefig([dez[11][1:-1],dez[14][1:-1],dez[13][1:-1],dez[16][1:-1],dez[10][1:-1],dez[22][1:-1]],'k')
+            w[abs(int(a))].append(np.log10(dez[10][-1]))
+            z[abs(int(a))].append(dez[22][-1])
+        plt.plot(z[abs(int(a))],w[abs(int(a))],sym[abs(int(a))]+'o')
+    plt.show()
+    return w,z
