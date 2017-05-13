ce416540e1904592f361f1a70c1f31f96ef4ae89
kiradust <kiradust@gmail.com>
2017-05-13 18:55:56 +0200

    various changes

--- regression.py
+++ regression.py
@@ -2,10 +2,12 @@ from pylab import polyfit
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import statsmodels.api as sm
+import numpy as np
 
-a = [0.35, 0, 0.42, 0.3720930233, 0.9733333333]
-b = [0.5584754086, -0.2071144338, 1.860188312, 0.5891017512, 1.35438295]
-
+a = [-0.35,0,-1,-0.3720930233,-0.9733333333,-0.187,-0.03633314701,-0.1677700348,-0.5700934579,-0.45,-0.43,0.7777777778]
+b = [0.5584754086,-0.2071144338,0.7318631339,0.5891017512,1.35438295,0.591474292,0.01766907449,0.369024698,1.505663297,0.4194874021,0.1993318498,-0.7007794937]
+absb = [6.350604546, 7.435346609, 12.75304637, 5.351872834, 10.2209239, 7.745351198, 29.3, 22, 7.1, 12.5]
+#b=absb
 print linregress(a,b)
 
 # Parameters:	
@@ -21,14 +23,17 @@ m,c = polyfit(a, b, 1)
 
 # p = polyfit(x,y,n) returns the coefficients for a polynomial p(x) of degree n that is a best fit (in a least-squares sense) for the data in y. The coefficients in p are in descending powers, and the length of p is n+1
 
-labels = ['25792562','19252497','21532577','25066727','20089897']
+#labels = ['25792562','19252497','21532577','25066727','20089897']
 
-plt.plot(a[0], b[0], 'ro',label='Whole cell patch clamp')
-plt.plot(a[1], b[1], 'ro')
-plt.plot(a[2], b[2], 'bo',label='Gramcidin perforated patch clamp')
-plt.plot(a[3], b[3], 'yo',label='Fluorescence imaging')
-plt.plot(a[4], b[4], 'go',label='Cell-attached patch clamp')
-plt.plot(a, m*a+c, color='black')
+#plt.plot(a[0], b[0], 'ro',label='Whole cell patch clamp')
+#plt.plot(a[1], b[1], 'ro')
+#plt.plot(a[2], b[2], 'bo',label='Gramcidin perforated patch clamp')
+#plt.plot(a[3], b[3], 'yo',label='Fluorescence imaging')
+#plt.plot(a[4], b[4], 'go',label='Cell-attached patch clamp')
+
+for i in xrange(len(a)):
+    plt.plot(a[i],b[i],'o')
+plt.plot(a, np.linalg.linalg.multiply(m,a)+c, color='black')
 
 plt.legend(loc='lower right')
 
